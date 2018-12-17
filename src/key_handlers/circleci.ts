0ab2a89afd407ab3fd8020e76295690dc3dfd2ae
Dom Parfitt <dominic.parfitt@economist.com>
2018-12-17 10:36:28 +0000

    Created standalone function for handling new key for CircleCI

---
+++ src/key_handlers/circleci.ts
@@ -0,0 +1,32 @@
+import { AccessKey } from "aws-sdk/clients/iam";
+
+export function sendNewKey(key: AccessKey) {
+    const url = `https://circleci.com/api/v1.1/project/
+                    ${process.env.VCS_PROVIDER}/
+                    ${process.env.VCS_USER}/
+                    ${process.env.PROJECT_NAME}/
+                    envvar?circle-token=${process.env.CIRCLECI_TOKEN}`;
+
+    const request = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: key.AccessKeyId,
+            value: key.SecretAccessKey,
+        }),
+    };
+    console.log(`Sending request to CircleCI: ${JSON.stringify(request)}`);
+
+    return fetch(url, request)
+        .then((resp) => {
+            console.log(`Received response from CircleCI: ${JSON.stringify(resp)}`);
+            // return Promise.resolve(resp);
+        })
+        .catch((err) => {
+            // return Promise.reject(err);
+            // TODO: What to do here? Retry? Can't rollback, discard key?
+            // this.handlerError(err);
+        });
+}
