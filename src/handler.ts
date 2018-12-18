d6e1a28d7d8faf64447031a7af80f0849b180142
Dom Parfitt <dominic.parfitt@economist.com>
2018-12-18 15:52:01 +0000

    Updated handler to use callback rather than returning response object

--- src/handler.ts
+++ src/handler.ts
@@ -5,27 +5,17 @@ import { KeyRotator } from "./keyRotator";
 
 export function rotateKeys(event: ScheduledEvent, context: Context, callback: Callback) {
 
-    // Closure for creating the response
-    const respond = (statusCode: number, message: string) => {
-        return {
-            statusCode,
-            body: JSON.stringify({
-                message,
-                input: event,
-            }),
-        };
-    };
-
     // Get the User
-    const user = process.env.CIRCLECI_IAM_USER;
+    const user = process.env.IAM_USER;
     if (!user) {
-        return respond(400, 'No IAM User Provided');
+        callback('No IAM User Provided');
+        return;
     }
 
     const iam = new IAM();
     const keyRotator = new KeyRotator(iam, sendKeyToCircleCI);
 
-    return keyRotator.rotateKeys(user)
-        .then(() => respond(200, `Successfully rotated Access Keys for ${user}`))
-        .catch((err) => respond(400, err));
+    keyRotator.rotateKeys(user)
+        .then(() => callback(null, `Successfully rotated Access Keys for ${user}`))
+        .catch((err) => callback(err));
 }
