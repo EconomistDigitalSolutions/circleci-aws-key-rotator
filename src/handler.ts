961aff405a0b5f07fd350c27b3ddb5d1dbd2561b
DomParfitt <dominicparfitt@economist.com>
2019-01-28 10:38:19 +0000

    Updated to use closure

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,8 @@
 import { KeyRotator } from "@economist/aws-key-rotator";
 import { Callback, Context, ScheduledEvent } from "aws-lambda";
 import { IAM } from "aws-sdk";
-import { sendKeyToCircleCI } from "./circleci";
+import { createCircleCIHandler } from "./circleci";
+import { BATCH } from "./mode";
 
 export function rotateKeys(event: ScheduledEvent, context: Context, callback: Callback) {
 
@@ -13,9 +14,34 @@ export function rotateKeys(event: ScheduledEvent, context: Context, callback: Ca
     }
 
     const iam = new IAM();
-    const keyRotator = new KeyRotator(iam, sendKeyToCircleCI);
+    const vcsProvider = process.env.VCS_PROVIDER!;
+    const vcsUser = process.env.VCS_USER!;
+    const project = process.env.PROJECT_NAME!;
+    const apiToken = process.env.API_TOKEN!;
+    const keyRotator = new KeyRotator(iam, createCircleCIHandler(
+        vcsProvider,
+        vcsUser,
+        project,
+        apiToken,
+        process.env.ACCESS_KEY_NAME,
+        process.env.SECRET_KEY_NAME));
 
     keyRotator.rotateKeys(user)
         .then(() => callback(null, `Successfully rotated Access Keys for ${user}`))
         .catch((err) => callback(err));
+
+    // if (process.env.MODE === BATCH) {
+    //     for (let i = 0; i < 0; i++) {
+    //         process.env.VCS_PROVIDER = "";
+    //         process.env.VCS_USER = "";
+    //         process.env.PROJECT_NAME = "";
+    //         process.env.API_TOKEN = "";
+
+    //         const rotator = new KeyRotator(iam, sendKeyToCircleCI);
+
+    //         rotator.rotateKeys(user)
+    //             .then(() => callback(null, `Successfully rotated Access Keys for ${user}`))
+    //             .catch((err) => callback(err));
+    //     }
+    // }
 }
