011ceafdce10de2d7d542cd4c516d8dc6d635db7
DomParfitt <dominicparfitt@economist.com>
2019-02-07 11:57:44 +0000

    Updated handler for batch jobs

--- src/handler.ts
+++ src/handler.ts
@@ -2,46 +2,34 @@ import { KeyRotator } from "@economist/aws-key-rotator";
 import { Callback, Context, ScheduledEvent } from "aws-lambda";
 import { IAM } from "aws-sdk";
 import { createCircleCIHandler } from "./circleci";
-import { getRotationJobs, RotationJob } from "./jobs";
-import { BATCH } from "./mode";
+import { getJobs, RotationJob } from "./jobs";
 
-export function rotateKeys(event: ScheduledEvent, context: Context, callback: Callback) {
-
+export async function batchRotate(event: ScheduledEvent, context: Context, callback: Callback) {
+    const jobs = await getJobs(process.env.REGION!, process.env.TABLE!);
     const iam = new IAM();
-
-    const jobs: Array<RotationJob> = getRotationJobs(process.env.MODE);
-    const results: Array<Promise<void>> = [];
+    const promises: Promise<void>[] = [];
+    const failedJobs: { user: string, reason: any }[] = [];
     for (const job of jobs) {
-
         const keyHandler = createCircleCIHandler(job.vcsProvider, job.vcsUser, job.project, job.apiToken, job.accessKeyName, job.secretKeyName);
         const keyRotator = new KeyRotator(iam, keyHandler);
 
-        results.push(keyRotator.rotateKeys(job.user));
+        const promise = keyRotator.rotateKeys(job.user)
+            .catch((err) => {
+                console.error(err);
+                failedJobs.push({
+                    user: job.user,
+                    reason: err,
+                });
+            });
+
+        promises.push(promise);
     }
 
-    Promise.all(results)
-        .then(() => callback(null, `Successfully rotated Access Keys for ${JSON.stringify(jobs.map((job) => job.user))}`))
-        .catch((err) => callback(err));
+    Promise.all(promises).then(() => {
+        if (failedJobs.length !== 0) {
+            callback(`Key rotation failed for the following users: ${JSON.stringify(failedJobs)}`);
+        }
 
-    //     } else {
-    //         // Get the User
-    //         const user = process.env.IAM_USER;
-    //         if (!user) {
-    //             callback('No IAM User Provided');
-    //             return;
-    //         }
-    //         const handler = createCircleCIHandler(
-    //             process.env.VCS_PROVIDER!,
-    //             process.env.VCS_USER!,
-    //             process.env.PROJECT_NAME!,
-    //             process.env.API_TOKEN!,
-    //             process.env.ACCESS_KEY_NAME,
-    //             process.env.SECRET_KEY_NAME);
-
-    //         const keyRotator = new KeyRotator(iam, handler);
-
-    //         keyRotator.rotateKeys(user)
-    //             .then(() => callback(null, `Successfully rotated Access Keys for ${user}`))
-    //             .catch((err) => callback(err));
-    //     }
+        callback(null, `Successfully rotated the Access Keys for: ${jobs.map((job) => job.user)}`);
+    });
 }
