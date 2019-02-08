423c64abdbec938bfe997bf1e07d6f7bb8aecdf1
DomParfitt <dominicparfitt@economist.com>
2019-02-08 14:51:14 +0000

    Added S3 injection

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,11 @@
 import { Callback, Context, ScheduledEvent } from "aws-lambda";
-import { IAM } from "aws-sdk";
+import { IAM, S3 } from "aws-sdk";
 import { batchRotateKeys } from "./batch";
 import { getJobsFromS3 } from "./jobs";
 
 export async function rotateKeys(event: ScheduledEvent, context: Context, callback: Callback) {
     try {
-        const jobs = await getJobsFromS3(process.env.BUCKET!);
+        const jobs = await getJobsFromS3(new S3(), process.env.BUCKET!);
         await batchRotateKeys(new IAM(), jobs)
             .then(() => callback(null, `Successfully completed key rotation.`));
     } catch (err) {
