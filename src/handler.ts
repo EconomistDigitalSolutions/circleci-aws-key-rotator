085eb8223b17ece140b3b5bcb1d51df0065dbb16
DomParfitt <dominicparfitt@economist.com>
2019-02-12 14:42:32 +0000

    Added handler for adding job

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
-import { Callback, Context, ScheduledEvent } from "aws-lambda";
+import { APIGatewayEvent, Callback, Context, ScheduledEvent } from "aws-lambda";
 import { IAM, S3 } from "aws-sdk";
 import { batchRotateKeys } from "./batch";
-import { getJobsFromS3 } from "./jobs";
+import { addJobToS3, getJobsFromS3 } from "./jobs";
 
 export async function rotateKeys(event: ScheduledEvent, context: Context, callback: Callback) {
     try {
@@ -9,6 +9,19 @@ export async function rotateKeys(event: ScheduledEvent, context: Context, callba
         console.log(`Retrieved jobs: ${JSON.stringify(jobs)}`);
         await batchRotateKeys(new IAM(), jobs);
         callback(null, `Successfully completed key rotation.`);
+    } catch (err) {
+        console.error(err);
+        callback(err);
+    }
+}
+
+export async function putJob(event: APIGatewayEvent, context: Context, callback: Callback) {
+    if (!event.body) {
+        throw new Error(`No body on request.`);
+    }
+
+    try {
+        await addJobToS3(new S3(), process.env.BUCKET!, event.body);
     } catch (err) {
         console.error(err);
         callback(err);
